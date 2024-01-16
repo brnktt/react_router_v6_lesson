d566cfab69150cb2e137ffdb1834b8cd7aa453b4
Alessandro Branchetti <brk.alessandro@gmail.com>
2024-01-16 12:00:11 +0100

    changed a tags to Link components

--- src/app/App.js
+++ src/app/App.js
@@ -16,7 +16,7 @@ import "./App.css";
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={ <Root/> }>
     <Route path="about" element={ <About/> } />
-    <Route path="sign-up" element={ <SignUp/> }/>
+    <Route path="sign-up" element={ <SignUp/> } />
     <Route path="articles" element={ <Articles/> } />
     <Route path="categories" element={ <Categories/> } />
     <Route path="profile" element={ <Profile/> } />
